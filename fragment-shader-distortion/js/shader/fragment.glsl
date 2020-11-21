cea83603728fc05eab5c2f435995bff47ff9d017
Martijn <martijn@buildinamsterdam.com>
2020-11-21 20:41:39 +0100

    tweaks

--- fragment-shader-distortion/js/shader/fragment.glsl
+++ fragment-shader-distortion/js/shader/fragment.glsl
@@ -15,14 +15,14 @@ void main() {
     vUv.x,
     vUv.y
   );
-
+ 
   displacedUV.y = mix(vUv.y, displace.r - 0.2, progress);
 
   vec4 color = texture2D(image, displacedUV);
 
   color.r = texture2D(image, displacedUV + vec2(0.,10.*0.005) * progress).r;
-  color.g = texture2D(image, displacedUV + vec2(0.,10.*0.001) * progress).g;
-  color.b = texture2D(image, displacedUV + vec2(0.,10.*0.002) * progress).b;
+  color.g = texture2D(image, displacedUV + vec2(0.,10.*0.02) * progress).g;
+  color.b = texture2D(image, displacedUV + vec2(0.,10.*0.01) * progress).b;
 
 
   gl_FragColor = color;
